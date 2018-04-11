5e7a4b82562eb391102bc00faceb45e76157d309
mfarooq28 <farooq.mscs846@iiu.edu.pk>
2018-04-11 17:11:22 +0500

    Add files via upload

--- SQLLite.py
+++ SQLLite.py
@@ -1,12 +1,30 @@
 import sqlite3
+from Employees import employees
+
 conn =sqlite3.connect('employees.db')
 curs = conn.cursor()
 #curs.execute("""CREATE TABLE employees (
 #                first text,
 #                last text,
 #                pay integer)""")
-#curs.execute("INSERT INTO employees VALUES ('Corey','Schafer', 5000)")
-curs.execute(" SELECT * FROM employees WHERE last = 'Schafer' ")
+
+#curs.execute("INSERT INTO employees VALUES ('Jhon','Wax', 7500)")
+#curs.commit()
+Emp1 = employees('James','Doe', 6500)
+Emp2 = employees('Jhon','Doe', 7500)
+
+print(Emp1.first, Emp1.last, Emp1.pay)
+print()
+print(Emp2.first, Emp2.last, Emp2.pay)
+print(Emp1.email)
+print()
+print(Emp2.email)
+print(Emp1.fullname)
+print()
+print(Emp2.fullname)
+curs.execute("INSERT INTO employees VALUES ('{}','{}','{}')".format(Emp1.first, Emp1.last, Emp1.pay))
+conn.commit()
+curs.execute(" SELECT * FROM employees ")
 print(curs.fetchall())
 conn.commit()
 conn.close()
